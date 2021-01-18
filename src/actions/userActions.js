26ba9b2eaa7398c85afe289a26268ecbc9ce30d7
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2021-01-18 20:48:33 +0530

    commit

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -7,7 +7,7 @@ export const register = (name, email, password) => async (dispatch) => {
         payload: { email, password}
     });
     try{
-        const {data} = await Axios.post('/api/users/register',{name, email, password});
+        const {data} = await Axios.post('https://dinesh-4r-spot.herokuapp.com/api/users/register',{name, email, password});
         dispatch({
             type: USER_REGISTER_SUCCESS,
             payload: data,
@@ -31,7 +31,7 @@ export const signin = (email, password) => async (dispatch) => {
         payload: { email, password}
     });
     try{
-        const {data} = await Axios.post('/api/users/signin',{email, password});
+        const {data} = await Axios.post('https://dinesh-4r-spot.herokuapp.com/api/users/signin',{email, password});
         dispatch({
             type: USER_SIGNIN_SUCCESS,
             payload: data,
